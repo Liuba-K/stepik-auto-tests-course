29cffb76c9bb98643e07e5e72f748a7559f272cb
Liuba Kundas <kundasl1@gmail.com>
2020-07-29 23:31:35 +0300

    change contains in failes

--- week_6/conftest.py
+++ week_6/conftest.py
@@ -1,15 +1,15 @@
 from logging import getLogger
 import pytest
-
+from faker import Faker
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from test_main_page import IMPLICIT_WAIT,WINDOW_SIZE
 logger = getLogger(__name__)
 
-def pytest_addoption(language):
-    language.adoption('--language',action='store',default=None, help='Choose interface language')
-    language.adoption('--browser', action='store', default='chrome', help='Choose browser')
+def pytest_addoption(parser):
+    parser.adoption('--language',action='store',default="en-gb", help='Choose interface language')
+    parser.adoption('--browser', action='store', default='chrome', help='Choose browser: chrome or firefox')
 
 @pytest.fixture
 def driver(request):
@@ -28,15 +28,17 @@ def driver(request):
     driver.set_window_size(*WINDOW_SIZE)
     yield driver
     driver.quit()
-@pytest.mark.parametrize('language', ["ru", "en-gb","ar","ca","cs","da","de","el","es","fi","fr","it","ko","nl" "pl","pt","pt-br","ro","sk","uk","zh-hans"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
-def browser():
-    print("\nstart browser for test..")
-    global browser
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+
+@pytest.fixture
+def test_email(request):
+    try:
+        params = request.param
+        if params == "free":
+            return Faker().ascii_free_email()
+        elif params == "company":
+            return Faker().ascii_company_email()
+        else:
+            logger.warning(f"incorrect request param:{params}")
+            return Faker().email()
+    except AttributeError:
+        return Faker().email()
